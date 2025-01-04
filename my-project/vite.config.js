0dabf515747922441e0d73ecd565f5417a959c6a
Ayoogunbanwo <ayoogunbanwo13@gmail.com>
2025-01-03 22:29:12 -0600

    #

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -2,9 +2,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer'; // Import the visualizer plugin
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({ open: true }) // Add the visualizer plugin here
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'), // Use '@' as an alias for 'src'
